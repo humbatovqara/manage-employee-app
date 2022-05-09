63565d723fc0458c0311fd724838bda6fb24e4a6
Qara <qarahumbtov@gmail.com>
2022-05-09 22:30:26 +0400

    Delete employee, useRef

--- src/components/Employee.js
+++ src/components/Employee.js
@@ -1,4 +1,10 @@
+import { useContext } from "react";
+import { EmployeeContext } from "../contexts/EmployeeContext";
+
 const Employee = ({ employees }) => {
+
+    const { deleteEmployee } = useContext(EmployeeContext)
+
     return (
         <>
             {
@@ -9,8 +15,8 @@ const Employee = ({ employees }) => {
                         <td>{employee.address}</td>
                         <td>{employee.phone}</td>
                         <td>
-                            <a href="#editEmployeeModal" className="edit" data-toggle="modal"><i className="material-icons" data-toggle="tooltip" title="Edit">&#xE254;</i></a>
-                            <a href="#deleteEmployeeModal" className="delete" data-toggle="modal"><i className="material-icons" data-toggle="tooltip" title="Delete">&#xE872;</i></a>
+                            <button className="btn text-warning p-0 edit" data-toggle="modal"><i className="material-icons" data-toggle="tooltip" title="Edit">&#xE254;</i></button>
+                            <button onClick={() => deleteEmployee(employee.id)} className="btn text-danger p-0 delete" data-toggle="modal"><i className="material-icons" data-toggle="tooltip" title="Delete">&#xE872;</i></button>
                         </td>
                     </tr>
                 ))
